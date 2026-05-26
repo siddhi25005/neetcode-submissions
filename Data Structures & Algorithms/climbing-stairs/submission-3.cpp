a39ea20b0e0c964f1030368b360aa4f9069f4175
siddhi25005 <tiwarisidddhi2005@gmail.com>
2026-05-27 04:14:53 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   int f(int n) {
+    if(n==1 || n==2) return n;
+    int a=1,b=2,c=0;
+    for(int i=2;i<n;i++) {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c;
+   }
+    int climbStairs(int n) {
+        return f(n);
+    }
+};
