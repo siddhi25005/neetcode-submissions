81e5231664063d9ff6bb0a50471df86471d8cb7a
siddhi25005 <tiwarisidddhi2005@gmail.com>
2026-05-28 02:48:29 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int n=arr.size();
+      
+        int maxi=-1;
+        for(int i=n-1;i>=0;i--) {
+          int curr=arr[i];
+               arr[i]=maxi;
+            maxi=max(maxi,curr);
+            
+       
+        }
+        arr[n-1]=-1;
+        return arr;
+        
+    }
+};
