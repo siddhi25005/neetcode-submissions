1d099000d8565c392e8260f9f40ae8dc19562054
siddhi25005 <tiwarisidddhi2005@gmail.com>
2026-07-10 02:19:11 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int v, vector<vector<int>>& arr1) {
+        vector<vector<int>> arr(v);
+
+        for(auto x : arr1) {
+            arr[x[1]].push_back(x[0]);
+        }
+
+        vector<int>indegree(v,0);
+        vector<int>ans;
+        queue<int>q;
+        for(int i=0;i<v;i++) {
+            for(int x:arr[i]) {
+                indegree[x]++;
+            }
+        }
+        for(int i=0;i<v;i++) {
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()) {
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(int x:arr[node]) {
+                indegree[x]--;
+                if(indegree[x]==0) q.push(x);
+            }
+        }
+        if(ans.size()==v) return true;
+        return false;
+    }
+};
